a1fca2f3aa546e510b62cd2de9abaefb083410b3
sik2 <devpang20@gmail.com>
2023-08-18 11:40:54 +0900

    setting thymeleaf and auth

--- src/main/java/com/cojar/market/product/controller/AdmProductController.java
+++ src/main/java/com/cojar/market/product/controller/AdmProductController.java
@@ -3,6 +3,7 @@ package com.cojar.market.product.controller;
 import com.cojar.market.product.entity.Product;
 import com.cojar.market.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +19,9 @@ import java.util.List;
 @RequestMapping("/adm/product")
 public class AdmProductController {
 
-
     private final ProductService productService;
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/create")
     public String create(Model model) {
         List<Product> productList = this.productService.getList();
@@ -29,6 +30,7 @@ public class AdmProductController {
         return "adm/product/create";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping("/create")
     public String createContent(
             @RequestParam String name, @RequestParam String description, @RequestParam int price,
